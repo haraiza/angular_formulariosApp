8148363834b1263f0a82db25d3c0e817066d726e
humberto-desk <humberto_araiza@outlook.com>
2022-02-04 20:56:44 -0800

    Submit del formulario

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
@@ -7,7 +7,7 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
   styles: [
   ]
 })
-export class BasicosComponent {
+export class BasicosComponent implements OnInit {
 
 
   // miFormulario: FormGroup = new FormGroup({
@@ -17,18 +17,38 @@ export class BasicosComponent {
   // })
 
   constructor(private fb: FormBuilder) { }
-  
+
+  ngOnInit() {
+    this.miFormulario.reset({
+      nombre: 'RTX 4080ti',
+      precio: 1500,
+      existencias: 10
+    })
+  }
+
+
   miFormulario: FormGroup = this.fb.group({
     nombre: [, [Validators.required, Validators.minLength(3)]],
     precio: [, [Validators.required, Validators.min(0)]],
     existencias: [, [Validators.required, Validators.min(0)]],
-    
+
   })
-  
+
   campoEsValido(campo: string) {
     return (
       this.miFormulario.controls[campo].errors &&
       this.miFormulario.controls[campo].touched
     )
+  }
+
+
+  guardar() {
+    if (this.miFormulario.invalid) {
+      this.miFormulario.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.miFormulario.value);
+    this.miFormulario.reset();
   }
 }
